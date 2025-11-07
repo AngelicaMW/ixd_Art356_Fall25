fc8d184378ef2b8cab7399e50009d0300189fcca
AngelicaMW <otakujelly@gmail.com>
2025-11-06 17:50:16 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,53 +1,104 @@
-let data;
-let url =
-  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSBPrE-wdPj8Oc1DU9wp_IrWyYEZwvEiIW6mV0oQRwe6hK00Ihja--5kPoarDCuf6eUXSdlgUWU0iu5/pub?gid=0&single=true&output=csv";
+//images and json
 
-let headerID = 0;
+let bugs;
+let tiger;
+let peacock;
+let agrias;
+let emperor;
+let common;
+let acbugarea;
+let bugsList;
+
+//the photos
 
 function preload() {
-  data = loadTable(url, "csv", "header");
+  acbugarea = loadImage("acbugarea.jpeg");
+  bugsList = loadJSON("bugs.json");
+  Common = loadImage("Common.png");
+  Tiger = loadImage("Tiger.png");
+  Peacock = loadImage("Peacock.png");
+  Agrias = loadImage("Agrias.png");
+  Emperor = loadImage("Emperor.png");
 }
+
+
+//loading the json
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1200, 800);
+  textAlign(CENTER);
+  textSize(20);
+
+  loadJSON("bugs.json", gotData);
+
+
 }
+
+function gotData(data){
+ bugsList = data;
+}
+
+
+ 
+//buterflies and background imgs
 
 function draw() {
-  background(100, 20, 20);
+  background(220);
+  imageMode(CORNER)
+  rectMode(CENTER)
 
-  if (data) {
-    let numRows = data.getRowCount(); //this grabs how many rows in the spreadsheet
 
-    let rowData = data.getRow(headerID); //this grabs a specific row within the dataset thanks to the headerID
+  image(acbugarea, 0, 0, 1200, 800);
 
-    textAlign(CENTER);
-    text(rowData.getString(0), width / 2, 50);
+    imageMode(CENTER)
+    textAlign(CENTER)
+  image(Common, 120, 350, 90, 40);
+  image(Tiger, 700, 190, 65, 30);
+  image(Peacock, 600, 600, 100, 50);
+  image(Agrias, 450, 240, 60, 65);
+  image(Emperor, 1000, 200, 80, 50);
 
-    for (let i = 0; i < numRows; i++) {
-      //we create some variables to help display our visuals
-      let x = 100 + i * 50;
-      let y = 200; //fancy math to space out shapes
-      // let w = rowData.getString(i + 1); //this is the width of our shape grabbing the data within the row using .getString()
-      let h = rowData.getString(i + 1);
 
-      rect(x, y, 10, h);
+//rect bg box
 
-      // ellipse(x, y, w); //this draws the shape. Don't forget fill() can be used!
 
-      //this displays the callouts for the shapes and data associated with it.
-      textAlign(RIGHT);
-      let textX = 100 + i * 65;
-      fill(520, 300, 50, 75);
-      textSize(14);
-      text(data.columns[i + 1], textX, y - 15);
-      textAlign(CENTER);
-      text(rowData.getString(i + 1), x, int(y) + int(h) + 25);
-      console.log();
-    }
+  if (mouseX > 70 && mouseX < 170 && mouseY > 300 && mouseY < 400 ) {
+  rect(120,350, 200, 150)
+  image(Common, 120, 350, 90, 40);
+  text(bugsList.common_butterfly.name["name-USen"], 120, 300);
+  text("Price " + bugsList.common_butterfly.price, 120, 400); 
   }
-}
+if (mouseX > 650 && mouseX < 750 && mouseY > 140 && mouseY < 240 ) {
+  rect(700, 190, 200, 150)
+  image(Tiger, 700, 190, 65, 30);
+  text(bugsList.tiger_butterfly.name["name-USen"], 700, 140);
+  text("Price " + bugsList.tiger_butterfly.price, 700, 240); 
 
-//this function basically pushes the headerID to parse through the different rows in the spreadsheet
-// function keyPressed() {
-//   headerID++;
-// }
+}
+if (mouseX > 550 && mouseX < 650 && mouseY > 550 && mouseY < 650 ) {
+  rect(600, 600, 200, 150)
+  image(Peacock, 600, 600, 100, 50);
+  text(bugsList.peacock_butterfly.name["name-USen"], 600, 550);
+  text("Price " + bugsList.peacock_butterfly.price, 600, 650); 
+
+}
+  
+if (mouseX > 400 && mouseX < 500 && mouseY > 190 && mouseY < 290 ) {
+  rect(450, 240, 200, 150)
+  image(Agrias, 450, 240, 60, 65);
+  text(bugsList.agrias_butterfly.name["name-USen"], 450, 190);
+  text("Price " + bugsList.agrias_butterfly.price, 450, 290); 
+
+}
+  
+if (mouseX > 950 && mouseX < 1050 && mouseY > 150 && mouseY < 250 ) {
+  rect(1000, 200, 200, 150)
+  image(Emperor, 1000, 200, 80, 50);
+  text(bugsList.emperor_butterfly.name["name-USen"], 1000, 150);
+  text("Price " + bugsList.emperor_butterfly.price, 1000, 250); 
+
+}
+  
+
+  }
+
